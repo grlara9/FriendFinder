1ca462315e45370255585b6f27f640eca8fe5a03
Guillermo Lara <grlara9@gmail.com>
2019-12-29 21:12:20 -0800

    work on home.html and survey.html

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,5 +1,11 @@
 
 module.exports = function(app){
-    
+    app.get("/api/friends", (req, res)=>{
+        res.json("hello")
+    })
+
+    app.post("/api/friends", (req, res)=> {
+        res.json("hello2")
+    })
     
 }
