72056ded756afcffb0a780017f3f6ce5172d6527
Guillermo Lara <grlara9@gmail.com>
2019-12-29 18:37:21 -0800

    setup htmlroutes.js server and home

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,6 +1,5 @@
 
 module.exports = function(app){
-    app.get("/", (req, res) => {
-        res.json("hello");
-    })
+    
+    
 }
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,4 +1,15 @@
+var path = require("path");
 
 module.exports= function(app){
+
+    //A GET Route to `/survey` which should display the survey page.
+    app.get("/survey", (req, res)=>{
+        res.sendFile(path.join(__dirname, "/../public/survey.html"));
+    })
+
+    //catch-all route that leads to `home.html` which displays the home page
+    app.get("*", (req, res)=> {
+        res.sendFile(path.join(__dirname, "/../public/home.html"))
+    })
     
 }
