32e0bf6d344874ad04f54cb4fd182760bc5aeed5
Guillermo Lara <grlara9@gmail.com>
2020-01-07 12:44:39 -0800

    finish editing ui for home and survey.html

--- server.js
+++ server.js
@@ -7,9 +7,14 @@ var app = express();
 //sets an initial port
 const PORT = process.env.PORT || 3000;
 
+// Expose the public directory to access CSS files
+app.use(express.static(path.join(__dirname, './app/public')));
+
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+
 
 require("./app/routing/apiRoutes")(app);
 require("./app/routing/htmlRoutes")(app);
